02588f8b65721b12747b199ae036f9188803c387
iigmir <roc120j@gmail.com>
2019-02-21 12:26:05 +0800

    Linting

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,17 +1,17 @@
-import { expect } from "chai"
-import { shallowMount } from "@vue/test-utils"
-import HelloWorld from "@/components/HelloWorld.vue"
+import { expect } from "chai";
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
     it("Renders props.msg when passed", () => {
-        const msg_cases = ["Hello World","hi"]
+        const msg_cases = ["Hello World","hi"];
         msg_cases.map( el =>
         {
             const wrapper = shallowMount(HelloWorld,
                 {
                     propsData: { msg: el }
-                })
-            expect(wrapper.text()).to.include(el)
-        })
-    })
-})
+                });
+            expect(wrapper.text()).to.include(el);
+        });
+    });
+});
